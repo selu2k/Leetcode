82e7bd727325b6ef3f50c0c0fea97e1e496715cc
Selukash <selu2kk@gmail.com>
2021-10-14 00:55:42 +0530

    Time: 24 ms (33.60%), Space: 5.8 MB (88.59%) - LeetHub

--- power-of-three/power-of-three.cpp
+++ power-of-three/power-of-three.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==1){return true;}
+         if(n==1){return true;}
         if(n==0){return false;}
-        if(n > 1162261467){return false;}
-        int last = n%10;
-        if( last == 1 || last == 3 || last == 7 || last == 9){
-            if(1162261467 % n == 0){
-                return true;
-            }
-            else{return false;}
+        while(n){
+            if(n%3 !=0){return false;}
+            n/=3;
+            if(n==1){return true;}
         }
         return false;
+        // if(n==1){return true;}
+        // if(n==0){return false;}
+        // if(n > 1162261467){return false;}
+        // int last = n%10;
+        // if( last == 1 || last == 3 || last == 7 || last == 9){
+        //     if(1162261467 % n == 0){
+        //         return true;
+        //     }
+        //     else{return false;}
+        // }
+        // return false;
     }
 };
