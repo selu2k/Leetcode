07750f96a3a4bac9fe4113d4e8d679eb2d11320b
Selukash <selu2kk@gmail.com>
2021-11-06 18:16:38 +0530

    Time: 8 ms (95.10%), Space: 14.1 MB (58.45%) - LeetHub

--- make-two-arrays-equal-by-reversing-sub-arrays/make-two-arrays-equal-by-reversing-sub-arrays.cpp
+++ make-two-arrays-equal-by-reversing-sub-arrays/make-two-arrays-equal-by-reversing-sub-arrays.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        unordered_multiset<int> t(target.begin(),target.end());
-        unordered_multiset<int> a(arr.begin(),arr.end());
-        if(t==a){return true;}
+        // unordered_multiset<int> t(target.begin(),target.end());
+        // unordered_multiset<int> a(arr.begin(),arr.end());
+        sort(target.begin(),target.end());
+        sort(arr.begin(),arr.end());
+        if(target==arr){return true;}
         return false;
     }
 };
