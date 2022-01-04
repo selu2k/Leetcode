a43eaa19dc31e0b6ccb9179d18330f65b66d26eb
Selukash <selu2kk@gmail.com>
2022-01-04 22:04:06 +0530

    Time: 6 ms (7.68%), Space: 6.1 MB (5.99%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     int bitwiseComplement(int num) {
         if(!num) return 1;
-        int l=floor(log2(num))+1;
-        int ress = num^((1L<<l)-1);
+        int l=floor(log2(num))+1; // number of bits needed
+        int temp = (1L<<l)-1;  // all bits are one
+         // 1L -> 1 in long notation and we shift it left by l then subtract 1 to 
+         // make all the bits required bits 1;
+        int ress = num^temp;
         return ress;
     }
 };
