12e04e26294a04632c160c19df46ed9aef9936c8
Selukash <selu2kk@gmail.com>
2022-06-04 18:56:11 +0530

    Time: 19 ms (65.36%), Space: 16 MB (26.22%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -4,11 +4,22 @@ public:
         int n = letters.size();
         int l=0;
         int r=n-1;
-        for(auto i : letters){
-            if( i > target){
-                return i;
+        int mid;
+        while(l<=r){
+            mid = l + (r-l)/2;
+            if(letters[mid]<= target){
+                l = mid+1;
+            }
+            else if(letters[mid] > target){
+                if( mid!=0 && letters[mid-1] <= target) return letters[mid];
+                r = mid-1;
             }
         }
+        // for(auto i : letters){
+        //     if( i > target){
+        //         return i;
+        //     }
+        // }
         return letters[0];
     }
 };
