008226f0e6069af956c1825e6f26d6b9ea8780fc
Selukash <selu2kk@gmail.com>
2022-06-25 14:53:46 +0530

    Time: 49 ms (27.45%), Space: 26.8 MB (97.48%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -3,21 +3,21 @@ public:
     bool checkPossibility(vector<int>& nums) {
         int n = nums.size();
         if(n<3) return true;
-        bool flag = false;
+        bool flag = false; // check for first error
         for(int i = 1 ; i < n ; i++ ){
-            if( nums [i] < nums[i-1]){
-                if(!flag){
-                    flag = true;
-                    if(i!=1){
-                        if(nums[i]<nums[i-2]){
+            if( nums [i] < nums[i-1]){ // if decreasing
+                if(!flag){ // if first offense
+                    flag = true; // mark true
+                    if(i!=1){ // if not first element (we can decrease the first one to minimum , so no issue)
+                        if(nums[i]<nums[i-2]){ // if current element less than the element before high) then we know we need to change this one otherwise false
                             nums[i] = nums[i-1];
                         }
-                        else{
+                        else{ // else we change the high point to element before coz its the one causing problems
                             nums[i-1] = nums[i-2];
                         }
                     }
                 }
-                else{
+                else{ // if second offense return false;
                     return false;
                 }
             }
