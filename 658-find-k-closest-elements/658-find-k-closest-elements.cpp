129603e39752c10d405de3bf08f0397d87db5f3c
Selukash <selu2kk@gmail.com>
2022-09-29 13:37:23 +0530

    Time: 119 ms (45.05%), Space: 31 MB (98.27%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-            stable_sort(arr.begin(),arr.end(),[x] (const auto a, const auto b){
-                return abs(a-x) < abs (b-x) ;
-            });
-            arr.resize(k);
-            sort(arr.begin(),arr.end());
-            return arr;
-//         int n = arr.size();
-//         int l = 0;
-//         int r = n - k;
-        
-//         while( l<r ){
-//             int m = (l + r)/2 ;
-//             if( x - arr[mid] > arr[mid+k] - x){
-                
-//             }
-//         }
+            // stable_sort(arr.begin(),arr.end(),[x] (const auto a, const auto b){
+            //     return abs(a-x) < abs (b-x) ;
+            // });
+            // arr.resize(k);
+            // sort(arr.begin(),arr.end());
+            // return arr;
+        int n = arr.size();
+        int l = 0;
+        int r = n - 1;
+         // two pointers from left and right // keep decreseing the window size till it fits
+        while( r-l >= k ){
+            if( x - arr[l] <= arr[r] - x) r--;
+            else l++;
+        }
+        return vector<int>(arr.begin()+l,arr.begin()+r+1);
     }
 };
